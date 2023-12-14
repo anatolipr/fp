c60e6d6b0d1b4ada2c155f4ee87e506a41276b47
anatolipr <anatoli@impact.com>
2023-12-14 10:25:51 -0800

    configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+//https://stackoverflow.com/questions/67781170/bundle-js-and-css-into-single-file-with-vite
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
+
+

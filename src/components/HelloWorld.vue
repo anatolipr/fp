9f5f32c42cf9979c6eeb8ccfc6dd96b936215155
anatolipr <anatoli+anatolipr@radulov.com>
2023-12-14 10:17:45 -0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<div
+    style="height: 47px; padding: 10px; font-size: 14px; border: 1px solid; border-radius: 10px; border-color: #42d2ff; display: flex; align-items: center; justify-content: center; margin-bottom: 10px">
+    <div style="flex: 1">
+        Cache advance offer of up to $1,400 has been pre-approved.
+    </div>
+    <div style="color: #42d2ff; cursor: pointer">Accept Offer</div>
+    <div
+        style="width: 19px; height: 17px; margin-left: 10px; cursor: pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+                fill="currentColor"
+                d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z" />
+        </svg>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+
+* {box-sizing: border-box}
+</style>

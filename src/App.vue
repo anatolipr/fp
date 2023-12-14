1eaf932bc0547488539158b071d6c809751ddd82
Anatoli Radulov <anatoli@impact.com>
2023-12-14 14:24:28 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-
-  <HelloWorld />
+    <div
+        style="height: 47px; padding: 10px; font-size: 14px; border: 1px solid; border-radius: 10px; border-color: #42d2ff; display: flex; align-items: center; justify-content: center; margin-bottom: 10px">
+        <div style="flex: 1">
+            Cache advance offer of up to $1,400 has been pre-approved.
+        </div>
+        <div style="color: #42d2ff; cursor: pointer" v-on:click="accept">
+            Accept Offer
+        </div>
+        <div
+            style="width: 19px; height: 17px; margin-left: 10px; cursor: pointer"
+            v-on:click="close">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path
+                    fill="currentColor"
+                    d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z" />
+            </svg>
+        </div>
+    </div>
 </template>
 
+<style scoped>
+
+
+    * {box-sizing: border-box}
+</style>
+
+<script setup>
+    function close() {
+        alert('closed')
+    }
+</script>

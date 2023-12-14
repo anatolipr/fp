31dc9fd67099a8b65064e36c35ad10941f74c1e5
anatolipr <anatoli@impact.com>
2023-12-14 11:13:11 -0800

    vite filename

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  rollupOutputOptions: {
+    entryFileNames: '[name].js',
+  },
   plugins: [vue(), cssInjectedByJsPlugin()],
   build: {
     rollupOptions: {

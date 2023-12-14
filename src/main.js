9265ee93a21c07ec4a06e5774a6bbe9885e5110a
anatolipr <anatoli@impact.com>
2023-12-14 13:58:57 -0800

    improvements

--- src/main.js
+++ src/main.js
@@ -5,18 +5,10 @@ import App from './App.vue'
 
 
 
-const getParamsFromURI = ( uri ) => {
-    // Get everything after the `?`
-    const [ , paramString ] = uri.split( '?' );
-  
-    // Return parameters
-    return new URLSearchParams( paramString );
-  };
-
 
 try {
-    var currentScript = document.currentScript?.src
-    const selector = getParamsFromURI(currentScript).get('s')
+    var currentScript = document.querySelector('#someUniqueId')
+    selector = currentScript.dataset.s
 
     document.querySelector(selector) //'#dashLeftCol'
         .insertAdjacentHTML('afterbegin', '<div id="app"></div>')

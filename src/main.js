0e1caafd247c7cd05049173b93ca50143defc1fd
anatolipr <anatoli@impact.com>
2023-12-14 13:40:04 -0800

    parameterized selector

--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+scriptSource = (function(scripts) {
+    var scripts = document.getElementsByTagName('script'),
+           script = scripts[scripts.length - 1];
+   if (script.getAttribute.length !== undefined) {
+      return script.src
+   }
+   return script.getAttribute('src', -1)
+}());
+
+const getParamsFromURI = ( uri ) => {
+    // Get everything after the `?`
+    const [ , paramString ] = uri.split( '?' );
+  
+    // Return parameters
+    return new URLSearchParams( paramString );
+  };
+
+const selector = getParamsFromURI(scriptSource()).get('s')
+
+document.querySelector(selector) //'#dashLeftCol'
+    .insertAdjacentHTML('afterbegin', '<div id="app"></div>')
+
 createApp(App).mount('#app')
